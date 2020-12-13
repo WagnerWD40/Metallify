94947348cff83177573fb9d67739f9ad6a254308
WagnerWD40 <wagnerwd40@gmail.com>
2020-12-13 16:06:11 -0300

    added bottomNavigationTab

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // remove notification bar
+        /*
+        this.getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+         */
+
         setContentView(R.layout.activity_main);
     }
 }
